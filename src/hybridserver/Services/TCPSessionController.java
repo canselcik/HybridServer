889fbb49df826a7ec9d977eaa23ca030bb1063ab
Can Selcik <selcik2@illinois.edu>
2012-07-21 17:26:31 +0300

    Passwords are now hashed with SHA-512

--- src/hybridserver/Services/TCPSessionController.java
+++ src/hybridserver/Services/TCPSessionController.java
@@ -3,6 +3,7 @@ package hybridserver.Services;
 import hybridserver.other;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import javax.xml.bind.DatatypeConverter;
 
 import configuration.runtime;
@@ -20,26 +21,41 @@ public class TCPSessionController{
 
 	public boolean isRecv = false;
 	public boolean isAuthenticated = false;
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public int evaluate(String msg, String remoteAddr) throws IOException {
 		if(!isAuthenticated){
 			// Since we don't have a lot of users and we don't need the user:key pairs to be
 			// changed often, we can just embed those into the code
-			if (msg.equals("can:12345") || msg.equals("jordan:12345")){
-				isAuthenticated = true;	
-				other.log(msg.split(":")[0] + " logged in");
-				send("+ AUTHENTICATED");
+			if(msg.contains(":")){
+				String[] pair = msg.split(":");
 				
-				return 1; // Keep-alive
-			}
-			
-			if(msg.equals("room:kp5g6d")){
-				isAuthenticated = true;
-				other.log("ROOM HAS JUST LOGGED IN -- necessary info will be forwarded to it (" + remoteAddr + ")");
+				HashMap d = new HashMap<String, String>();
+				d.put("can", "3627909A29C31381A071EC27F7C9CA97726182AED29A7DDD2E54353" +
+							"322CFB30ABB9E3A6DF2AC2C20FE23436311D678564D0C8D305930575F60E2D3D048184D79");
+				d.put("jordan", "3627909A29C31381A071EC27F7C9CA97726182AED29A7DDD2E54353" +
+						"322CFB30ABB9E3A6DF2AC2C20FE23436311D678564D0C8D305930575F60E2D3D048184D79");
+				d.put("room", "61F1559B07878560A72E897573621B5EFC34DAA75908F20E0046AF9FB8" +
+							"61192425614E7CBCAAF107359FCE7E77B425704CDDC7CA5F523203B986A802E433AF7F");
 				
-				runtime.setRoomPipe(stream);
-				send("+ ROOM_STATUS_ASSIGNED");
-				
-				return 1;
+				if(pair.length == 2){
+					String hash = other.getSHA512Hash(pair[1]).toUpperCase();
+					
+					if(d.get(pair[0]).equals(hash)){ // Hash matches
+						isAuthenticated = true;	
+						
+						if(pair[0].equals("room")){ // It is the room logging in
+							other.log("ROOM HAS JUST LOGGED IN -- necessary info will be forwarded to it (" + remoteAddr + ")");
+							
+							runtime.setRoomPipe(stream);
+							send("+ ROOM_STATUS_ASSIGNED");
+						}
+						else{ // It is a user logging in
+							other.log(msg.split(":")[0] + " logged in");
+							send("+ AUTHENTICATED");
+						}
+						return 1; // Keep-alive
+					}
+				}
 			}
 			
 			other.log("FAILED log-in attempt");
@@ -53,7 +69,7 @@ public class TCPSessionController{
 		}
 		else if ( msg.startsWith("broadcast ")) {  // Broadcast data to the room -- for debugging purposes mostly
 			other.log("Data relayed to the room: " + msg.split(" ")[1]);
-			runtime.broadcast(msg.split(" ")[1]);
+			runtime.broadcast("+ EVENT " + msg.split(" ")[1]);
 		}
 		else if (msg.equals("status") ){
 			other.log(remoteAddr + " asked for server status data");
